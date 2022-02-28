056c514ba7ff955e5ee6da568b69c02513719fcd
kamaldalakoti <64273320+kamaldalakoti@users.noreply.github.com>
2022-02-28 15:51:01 +0530

    Add files via upload

---
+++ bhartiya_airways/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for bhartiya_airways project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bhartiya_airways.settings')
+
+application = get_asgi_application()
